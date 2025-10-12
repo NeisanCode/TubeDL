6a818f89a067d3c9533e6234d2af1c7bf0069ca2
Neisan <misterneisan0@gmail.com>
2025-10-12 18:53:13 +0100

    updates

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import tkinter.filedialog as tkf
 import json
 
 
-def download_video(url):
+def download_video(url, dir=""):
     options = {
         # ⚠️ Supprimé "force_generic_extractor": True -> peut empêcher yt-dlp d’utiliser le bon extracteur YouTube
         "extractor_args": {"youtube": ["player_client=android_lite"]},
@@ -19,7 +19,7 @@ def download_video(url):
             "Chrome/118.0.0.0 Safari/537.36"
         },
         "format": "bv*[ext=mp4]+ba[ext=m4a]/b",  # meilleur combo mp4+m4a
-        "outtmpl": "%(title)s.%(ext)s",
+        "outtmpl": f"{dir}%(title)s.%(ext)s",
         "noplaylist": True,
         "quiet": False,  # pour afficher la progression
         "merge_output_format": "mp4",
@@ -29,10 +29,10 @@ def download_video(url):
         ydl.download([url])
 
 
-def download_playlist(url):
+def download_playlist(url, dir=""):
     options = {
         "format": "bv*+ba/b",
-        "outtmpl": "Playlists/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s",
+        "outtmpl": f"{dir}Playlists/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s",
         "yesplaylist": True,
         "quiet": False,
         "merge_output_format": "mp4",
@@ -44,10 +44,10 @@ def download_playlist(url):
 
 def media_path():
     home_directory = Path.home()
-    media_path = tkf.askdirectory(
+    return tkf.askdirectory(
         title="Repertoire de téléchargement", initialdir=home_directory
     )
-    return media_path
+
 
 def restart_app(title):
     print(Fore.LIGHTGREEN_EX + "Redemarrage en cours".center(50, "-"))
@@ -74,29 +74,34 @@ init(autoreset=True)
 print(Fore.LIGHTRED_EX + title)
 print("1 - Télécharger une vidéo\n2 - Télécharger une playlist\n3 - Quitter")
 
-actions = {
-    "1": lambda url: download_video(url),
-    "2": lambda url: download_playlist(url),
+app_actions = {
+    "1": lambda url, path: download_video(url, path),
+    "2": lambda url, path: download_playlist(url, path),
     "3": lambda _: close_app(),
 }
-restarter = {"o": lambda t: restart_app(t), "n": lambda _: close_app()}
-root = tk.Tk
-root.lift
+res_actions = {"o": lambda t: restart_app(t), "n": lambda _: close_app()}
 
-# while True:
-#     choice = input(Fore.LIGHTGREEN_EX + "Choisissez une option (1, 2, 3): ").strip()
-#     if choice in actions:
-#         if choice != "3":
-#             url = input("Entrez l’URL de la vidéo ou de la playlist -> ").strip()
-#             actions[choice](url)
-#             restart = input(Fore.LIGHTGREEN_EX + "Voulez vous continuez ? (o/n) -> ")
-#             if restart in restarter:
-#                 restarter[restart](title)
-#             else:
-#                 close_app()
-#         else:
-#             actions[choice](None)
-#     else:
-#         print(Fore.LIGHTRED_EX + "Choix invalide !!!")
+root = tk.Tk()
+root.withdraw()
+root.attributes("-topmost", True)
+
+while True:
+    choice = input(Fore.LIGHTGREEN_EX + "Choisissez une option (1, 2, 3): ").strip()
+    if choice in app_actions:
+        if choice != "3":
+            url = input("Entrez l’URL de la vidéo ou de la playlist -> ").strip()
+            path = input("Choisir la destination du media (o/n) ? -> ").strip()
+            if path == "o":
+                path = media_path()
+            app_actions[choice](url, path)
+            restart = input(Fore.LIGHTGREEN_EX + "Voulez vous continuez ? (o/n) -> ")
+            if restart in res_actions:
+                res_actions[restart](title)
+            else:
+                close_app()
+        else:
+            app_actions[choice](None)
+    else:
+        print(Fore.LIGHTRED_EX + "Choix invalide !!!")
 
 media_path()
